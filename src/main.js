8320e4766d753dda72dff7023790f7491c44e8b2
hellostarfish <hellostarfish@163.com>
2017-03-07 09:16:04 +0800

    完成Header部分

--- src/main.js
+++ src/main.js
@@ -1,14 +1,92 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App.vue';
+Vue.config.productionTip = false;
+import VueRouter from 'vue-router';
+import './common/stylus/index.styl';
+import VueResource from 'vue-resource';
 
-/* eslint-disable no-new */
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+//定义（路由）组件
+import goods from 'components/goods/goods';
+import seller from 'components/seller/seller';
+import ratings from 'components/ratings/ratings';
+
+//定义路由
+let routes = [
+    {path: '/goods', component: goods},
+    {path: '/seller', component: seller},
+    {path: '/ratings', component: ratings},
+];
+
+//创建router实例
+let router = new VueRouter({
+    base:'/goods',
+    linkActiveClass:'active',
+    routes
+});
+
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
-})
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: {App}
+});
+
+// router.go('/goods');
+
+/*
+ import goods from './components/goods/goods';
+ import seller from './components/seller/seller';
+ import ratings from './components/ratings/ratings';
+
+
+ Vue.use(VueRouter);
+
+ let app=Vue.extend(App);
+ let router=new VueRouter();
+
+ router.map({
+ '/goods':{
+ component:goods,
+ },
+ '/seller':{
+ component:seller,
+ },
+ '/ratings':{
+ component:ratings,
+ }
+ });
+
+ router.start(app,'#app');
+
+
+
+
+ */
+/*
+
+ let app = Vue.extend(App);
+
+ let router = new VueRouter({
+
+ });
+
+ router.map({
+ '/goods': {
+ component: goods
+ },
+ '/ratings': {
+ component: ratings
+ },
+ 'seller': {
+ component: seller
+ }
+ });
+
+ router.start(app, '#app');
+
+ router.go('/goods');*/
